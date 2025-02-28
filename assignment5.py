f8f1736607b436b513e4fa2ce7c764400765289f
aksarxxi <aksar8030@gmail.com>
2025-02-28 12:39:26 +0530

    added

---
+++ assignment5.py
@@ -0,0 +1,6 @@
+# Sum of first N natural numbers
+N = int(input("Enter a number: "))
+
+sum_of_numbers = (N * (N + 1)) // 2
+
+print(f"The sum of the first {N} natural numbers is: {sum_of_numbers}")
